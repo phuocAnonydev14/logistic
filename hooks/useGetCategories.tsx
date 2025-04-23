7f7f6fc7d0f6cd95f460010362e58e7c5ca3402b
anonyDev <phuochh@nortstudio.vn>
2025-04-23 23:55:17 +0700

    sdasd

--- hooks/useGetCategories.tsx
+++ hooks/useGetCategories.tsx
@@ -2,14 +2,23 @@
 
 import {useQuery} from "@tanstack/react-query";
 import {CategoryGetAllFilter, categoryService} from "@/services/category.service";
+import {Category} from "@/types/app.type";
+import {useAtom, atom} from 'jotai'
 
-export const useGetCategories = ({filter}:{filter?:CategoryGetAllFilter}) => {
-  return useQuery({
-    queryKey: ["categories"], // Include filter in queryKey for proper caching
-    queryFn: () => categoryService.getAllCategories(filter),
-    staleTime: 5 * 60 * 1000, // 5 minutes in milliseconds
-    refetchOnWindowFocus: false, // Optional: prevents refetch on window focus
-    refetchOnMount: false, // Optional: prevents refetch when component mounts
-    refetchOnReconnect: false // Optional: prevents refetch on network reconnect
-  });
+const categoryAtom = atom<Category[]>([])
+
+export const useGetCategories = ({filter}: { filter?: CategoryGetAllFilter }) => {
+	
+	const [category, setCategory] = useAtom(categoryAtom)
+	
+	return useQuery({
+		queryKey: ["categories"], // Include filter in queryKey for proper caching
+		queryFn: async () => {
+			if (category.length) return category
+			const res = await categoryService.getAllCategories(filter)
+			setCategory(res)
+			return res
+		},
+		staleTime: 5 * 60 * 1000, // 5 minutes in milliseconds
+	});
 }
